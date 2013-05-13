f73fdb0b1b531b15c2ce25ec469e500dc3272650
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2013-05-13 22:44:59 +0000

    Rm comments, fmt<

--- archive.php
+++ archive.php
@@ -1,31 +1,27 @@
 <?php get_header(); ?>
 
-<div class="content-title">
+    <div class="content-title">
 
-    <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+        <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
                 
-    <?php /* If this is a tag archive */ if ( is_tag() ) { ?>
-    <?php printf(__('<div class="archive">&raquo; Greinar í flokknum &bdquo;%s&ldquo;</div>'), single_tag_title('', false) ); ?>
-    <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-    <?php printf(_c('<div class="archive"> Greinasafn %s</div>'), get_the_time(__('M j, Y'))); ?>
-    <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-    <?php printf(_c('<div class="archive"> Greinasafn %s</div>'), get_the_time(__('F, Y'))); ?>
-    <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-    <?php printf(_c('<div class="archive"> Greinasafn %s</div>'), get_the_time(__('Y'))); ?>
-    <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-    <?php printf(__('<div class="archive"> &raquo; Greinar eftir %s</div>'), get_the_author_meta('name2') ); ?>
-    <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-    <?php _e('Arkífur'); ?>
-    <?php } ?>
+        <?php /* If this is a tag archive */ if ( is_tag() ) { ?>
+        <?php printf(__('<div class="archive">&raquo; Greinar í flokknum &bdquo;%s&ldquo;</div>'), single_tag_title('', false) ); ?>
+        <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
+        <?php printf(_c('<div class="archive"> Greinasafn %s</div>'), get_the_time(__('M j, Y'))); ?>
+        <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+        <?php printf(_c('<div class="archive"> Greinasafn %s</div>'), get_the_time(__('F, Y'))); ?>
+        <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+        <?php printf(_c('<div class="archive"> Greinasafn %s</div>'), get_the_time(__('Y'))); ?>
+        <?php /* If this is an author archive */ } elseif (is_author()) { ?>
+        <?php printf(__('<div class="archive"> &raquo; Greinar eftir %s</div>'), get_the_author_meta('name2') ); ?>
+        <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+        <?php _e('Arkífur'); ?>
+        <?php } ?>
 
-</div> 
+    </div>
 
+    <?php get_template_part('loop-greinasafn'); ?>
 
-
-<?php get_template_part('loop-greinasafn'); ?>
-
-
-<?php get_template_part('pagination'); ?>
-
+    <?php get_template_part('pagination'); ?>
 
 <?php get_footer(); ?>
