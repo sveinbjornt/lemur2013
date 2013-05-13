f1a2ed14181eda00a059ec4d32a1e7144390ee35
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2013-05-13 22:45:19 +0000

    fmt

--- bordi.php
+++ bordi.php
@@ -1,9 +1,9 @@
 <div class="grid gutter collapse720 bordi">
-	<?php $my_query = new WP_Query('cat=597&showposts=3'); ?>
-	
-	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-    	<div class="col s1of3 bordi-item">
-		    
+    <?php $my_query = new WP_Query('cat=597&showposts=3'); ?>
+    
+    <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+        <div class="col s1of3 bordi-item">
+            
             <div class="thumb">
                 <a class="aspect a275-200" href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
                     <?php the_post_thumbnail('sidebar'); ?>
@@ -11,16 +11,15 @@
             </div>
 
             <div class="main">
-            	<h2>
-            	    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-            	</h2>
-
-
-            	<p><?php
+                <h2>
+                    <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                </h2>
+                <p><?php
                 global $post;
                 echo get_post_meta($post->ID, 'bordi', true);
                 ?></p>
-            </div>	
+            </div>
+            
         </div>
     <?php endwhile; ?>
 
