b5d61f9bbf45c6dea90218cd9c3d16036b587b10
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2014-04-17 17:26:28 +0000

    Changed behavior and appearance of pagination

--- pagination.php
+++ pagination.php
@@ -1,10 +1,15 @@
 <?php if (get_option('paging_mode') == 'default') : ?>
     <div class="pagination grid">
-        <div class="col s1of2">
+        <div class="col s1of1">
         <?php previous_posts_link(__('<< Nýrra')); ?>
-        </div>
-        <div class="col s1of2">
         <?php next_posts_link(__('Eldra >>')); ?>
+        <p class="pagecount"> <?php
+            $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            if ($page != 1) {
+                echo "Síða " . $page;
+            }
+        ?></p>
+        
         </div>
     </div>
     <?php if (function_exists('wp_pagenavi')) wp_pagenavi(); ?>
