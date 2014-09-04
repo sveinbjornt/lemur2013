72739722dc96248475509d2d722e42df19df4292
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2014-09-04 01:49:17 +0000

    Adding Modernus Samræmd Vefmæling

--- footer.php
+++ footer.php
@@ -50,6 +50,21 @@
         <?php wp_footer(); ?>        
         <script src="<?php echo get_template_directory_uri() ?>/assets/js/lemur.js" defer></script>
         
+        <!-- Virk vefmaeling byrjar. Vefur: http://lemurinn.is -->
+
+        <script>
+        	is_teljari_potency = {};
+        	is_teljari_potency.service_id = 16439;
+        	is_teljari_potency.portion = "NÚMER VEFHLUTA";
+
+        </script>
+        <script src="http://potency-cnt.teljari.is/potency/js/potency.js"></script>
+        <noscript>
+        <img width="1" height="1" src="http://potency-cnt.teljari.is/potency/potency.php?o=16439;i=NÚMER VEFHLUTA;p=SÍÐUNAFN" alt="" border="0" />
+        </noscript>
+
+        <!-- Virk vefmaeling endar -->
+        
     </body>
 
 </html>
