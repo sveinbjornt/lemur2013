be539fa4ae2eeff4319684db7f577e591ba67e0e
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2014-10-05 19:27:14 +0000

    Changed 404 page

--- 404.php
+++ 404.php
@@ -2,23 +2,28 @@
 <head>
     <title>Lemúrinn | 404 | Síða fannst ekki</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body style="background-color: black; color: #eee;">
-
-<center>
-<br>
-<br>
-<img src="<?php bloginfo('template_url'); ?>/assets/images/404-lemur.jpg" />
-<h1>404</h1>
-<h2>Umbeðin síða fannst ekki.</h2>
-<p> <a href="javascript:
-    if (document.referrer == '') {
-        location.href = 'http://lemurinn.is';
-    } else {
-        history.back()
+    <style type="text/css">
+    img {
+        margin: 0 auto;
+        margin-top: 40px;
+        max-width: 100%;
+        height: auto;
     }
-    " style="color: #eee;">&lt;-- Aftur á Lemúrinn</a></p>
-</center>
+    
+    body {
+        margin: 0 auto;
+        background-color: black;
+        text-align: center;
+        padding: 0;
+    }
+    
+    </style>
+</head>
+<body>
+
+    <img src="<?php bloginfo('template_url'); ?>/assets/images/lemur-404.png" width="445" height="340"/>
 
 </body>
 </html>
+
+“
