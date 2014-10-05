ed949ca3068692fe902f95b1e1ffb348c96b63d2
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2014-10-05 19:28:07 +0000

    Disable twenty twenty

--- footer.php
+++ footer.php
@@ -49,9 +49,7 @@
         <?php wp_footer(); ?>        
         <script src="<?php echo get_template_directory_uri() ?>/assets/js/lemur.js" defer></script>
         
-        <?php //if (is_single() and in_category('slider')): ?>
-            <script src="<?php echo get_template_directory_uri() ?>/assets/js/jquery.twentytwenty-combined.js"></script>
-        <?php //endif; ?>
+            <!--<script src="<?php echo get_template_directory_uri() ?>/assets/js/jquery.twentytwenty-combined.js"></script>-->
     </body>
 
 </html>
