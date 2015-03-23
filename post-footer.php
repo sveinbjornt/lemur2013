86fbad48eb4990c4baf6bdd6ae4e48ed0bad6df8
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-03-23 07:08:31 +0000

    Fixed issue where FB comments embed didn't resize responsively

--- post-footer.php
+++ post-footer.php
@@ -6,7 +6,7 @@
     <div class="fb-like" data-href="<?php the_permalink() ?>" data-send="false" data-action="like" data-width="670" data-show-faces="false" data-colorscheme="<?php if ( in_category(26) ) { echo 'dark'; } else { echo 'light'; } ?>" data-layout="standard"></div>
 
     <div class="comments">
-        <div class="fb-comments" data-href="<?php the_permalink(); ?>" style="width: 100%;" data-width="670" data-num-posts="10" data-colorscheme="<?php if ( in_category(26) ) { echo 'dark'; } else { echo 'light'; } ?>"></div>
+        <div class="fb-comments" data-href="<?php the_permalink(); ?>" data-width="100%" data-num-posts="10" data-colorscheme="<?php if ( in_category(26) ) { echo 'dark'; } else { echo 'light'; } ?>"></div>
     </div>
     
 </div>
