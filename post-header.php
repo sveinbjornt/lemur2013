91521bc33c187ce9a56541a8c199c3d6ce72b9d4
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-03-27 12:37:30 +0000

    Show FB like box in loop for logged in users

--- post-header.php
+++ post-header.php
@@ -7,7 +7,7 @@
 
 <div class="post-meta grid">
     
-    <div class="col <?php echo (is_single()) ? "s2of3" : "s3of3"; ?> author-box">
+    <div class="col <?php echo (is_single() or is_user_logged_in()) ? "s2of3" : "s3of3"; ?> author-box">
         eftir
 	    <a class="author" href="<?php $id = get_the_author_meta('ID'); echo get_author_posts_url($id); ?>" title="Greinar eftir <?php the_author_meta( 'name2' ); ?>">
 	        <?php the_author_meta('name2'); ?> 
@@ -17,7 +17,7 @@
 	    </span>
 	</div>
 	
-	<?php if (is_single()): ?>
+	<?php if (is_single() or is_user_logged_in()): ?>
 	<div class="col s1of3 facebook-like">
 	    <div class="fb-like" data-action="like" data-href="<?php the_permalink() ?>" data-send="false" data-width="60" data-show-faces="false" data-colorscheme="<?php if ( in_category(26) ) { echo 'dark'; } else { echo 'light'; } ?>" data-layout="button_count"></div>
     </div>
