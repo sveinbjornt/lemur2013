b82c552d32d2765712df8c21b71a3fd8039dba9f
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-04-04 20:50:23 +0000

    Don't show any results if no ID is passed for single gallery item

--- template-gallery-item.php
+++ template-gallery-item.php
@@ -5,8 +5,10 @@
 ?>
 <?php
     $gallery_post_id = $_GET['gallery_post_id'];
-    query_posts('p=' . $gallery_post_id);
 ?>
-<div style="padding: 20px;">
-<?php get_template_part('single-post-content'); ?>
-</div>
+<?php if ($gallery_post_id): ?>
+    <? query_posts('p=' . $gallery_post_id); ?>
+    <div style="padding: 20px;">
+    <?php get_template_part('single-post-content'); ?>
+    </div>
+<? endif; ?>
