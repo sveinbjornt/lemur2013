b2f01be794641d47690acf3a9a2367d9ecb88888
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-05-20 23:30:44 +0000

    Removed unused code

--- template-hladvarp-feed.php
+++ template-hladvarp-feed.php
@@ -16,7 +16,6 @@ echo '
     <language>is-is</language>
     <lastBuildDate>Sat, 25 Mar 2006 11:30:00 -0500</lastBuildDate>
     <pubDate>Sat, 25 Mar 2006 11:30:00 -0500</pubDate>
-    <docs>http://blogs.law.harvard.edu/tech/rss</docs>
     <webMaster>lemurinn@lemurinn.is</webMaster>
 
     <itunes:author>Lemúrinn</itunes:author>
@@ -32,30 +31,6 @@ echo '
         <itunes:category text="Podcasting"/>
     </itunes:category>
 
-    <item>
-    <title>411 Item 138 ZedCast with Bruce Murray - Voicemail line 206-666-4357 </title>
-    <link>http://podcast411.com/forums/viewtopic.php?t=451</link>
-    <guid>http://media.libsyn.com/media/podcast411/411_060325.mp3</guid>
-    <description> Welcome to the show it is March 25th and this is our 138th 
-       show.  Today will be an interview with Bruce Murray from the Zedcast 
-       podcast. Please visit this podcast at http://www.zedcast.com/ </description>
-    <enclosure url="http://media.libsyn.com/media/podcast411/411_060325.mp3" length="11779397" type="audio/mpeg"/>
-    <category>Podcasts</category>
-    <pubDate>Sat, 25 Mar 2006 11:30:00 -0500</pubDate>
-
-    <itunes:author>Rob @ podCast411</itunes:author>
-
-    <itunes:explicit>No</itunes:explicit>
-    <itunes:subtitle>Welcome to the show it is March 25th and this is our 138th 
-       show.  Today will be an interview with Bruce Murray from the Zedcast 
-       podcast. Please visit this podcast at http://www.zedcast.com/ </itunes:subtitle>
-    <itunes:summary> Welcome to the show it is March 25th and this is our 
-       138th show.  Today will be an interview with Bruce Murray from the 
-       Zedcast podcast. Please visit this podcast at http://www.zedcast.com/ </itunes:summary>
-    <itunes:duration>00:24:30</itunes:duration>
-    <itunes:keywords>podCast411, podcast, podcasting, podcaster, Interviews, Bruce Murray, Zedcast</itunes:keywords>
-
-    </item>
 </channel>
 
 </rss>
