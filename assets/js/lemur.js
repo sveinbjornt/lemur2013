becf873adabd5cd0d8a985ed5a244dc8efb5d31b
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-06-10 21:32:39 +0000

    Removed Google Analytics

--- assets/js/lemur.js
+++ assets/js/lemur.js
@@ -11,21 +11,6 @@ var yt_link = function(ytid, list_id) {
     return tpl;
 };
 
-////////////// Google Analytics //////////////
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-25230683-1']);
-_gaq.push(['_trackPageview']);
-
- (function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl': 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-
-
 ////////////// Facebook Initialization //////////////
  (function(d, s, id) {
     var js,
